212ede62dad9db5f368cbaf8598972fc10183cc3
alflennik <alflennik@users.noreply.github.com>
2023-05-16 14:42:19 -0400

    Work on site deployment

---
+++ site-hosting/site-build.js
@@ -0,0 +1,112 @@
+const fs = require("fs/promises")
+const path = require("path")
+const { exec } = require("child_process")
+
+const assetLocations = ["tunes.html", "songs", "components", "library", "utilities"]
+
+const siteBuildScript = async ({ environment }) => {
+  if (environment !== "staging" && environment !== "production") {
+    throw new Error("Please specify environment")
+  }
+
+  const checkGit = async () => {
+    const getStatus = async () => {
+      const status = await new Promise((resolve, reject) => {
+        exec("git status", (error, stdout, stderr) => {
+          if (error || stderr) {
+            return reject(error || stderr)
+          }
+          resolve(stdout)
+        })
+      })
+
+      const branch = status.match(/On branch ([^\n]+)\n/)[1]
+      const isWorkingTreeClean = status.match(/nothing to commit, working tree clean/) !== null
+      const isUpToDate = status.match(/Your branch is up to date/) !== null
+
+      return { branch, isWorkingTreeClean, isUpToDate }
+    }
+
+    const checkBranches = async () => {
+      const branches = await new Promise((resolve, reject) => {
+        exec("git branch", (error, stdout, stderr) => {
+          if (error || stderr) {
+            return reject(error || stderr)
+          }
+          resolve(stdout)
+        })
+      })
+
+      if (branches.match(`\\s${environment}\\n`) === null) {
+        throw new Error(`Your ${environment} branch is missing`)
+      }
+    }
+
+    await checkBranches()
+
+    const { branch, isWorkingTreeClean, isUpToDate } = await getStatus()
+
+    if (branch !== "main") {
+      throw new Error("You are not currently on the main branch")
+    }
+
+    if (!isWorkingTreeClean) {
+      throw new Error("You have uncommitted changes")
+    }
+
+    if (!isUpToDate) {
+      throw new Error("You need to push your changes")
+    }
+
+    const checkoutBranch = async (branch) => {
+      await new Promise((resolve, reject) => {
+        exec(`git checkout ${branch}`, (error, stdout, stderr) => {
+          if (error || stderr) {
+            return reject(error || stderr)
+          }
+          resolve()
+        })
+      }).catch((error) => {
+        console.error(error)
+        throw new Error("Failed to checkout")
+      })
+
+      const { branch: endingBranch } = await getStatus()
+
+      if (endingBranch !== branch) throw new Error("Failed to checkout")
+    }
+
+    await checkoutBranch(environment)
+  }
+
+  await checkGit()
+
+  const buildFolder = path.resolve(__dirname, "public")
+
+  const clearBuildFolder = async () => {
+    const fileNames = await fs.readdir(buildFolder)
+    await Promise.all(
+      fileNames.map(async (fileName) => {
+        const filePath = path.resolve(buildFolder, fileName)
+        console.log(filePath)
+        await fs.rm(filePath, { recursive: true })
+      })
+    )
+  }
+
+  await clearBuildFolder()
+
+  const copyAssets = async () => {
+    await Promise.all(
+      assetLocations.map(async (assetLocation) => {
+        const assetSource = path.resolve(__dirname, "../", assetLocation)
+        const assetDestination = path.resolve(buildFolder, assetLocation)
+        fs.cp(assetSource, assetDestination, { recursive: true })
+      })
+    )
+  }
+
+  await copyAssets()
+}
+
+module.exports = siteBuildScript
