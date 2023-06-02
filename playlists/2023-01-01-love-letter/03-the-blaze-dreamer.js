fbcade426ef13961998a8154d550fdcc0cfc92d8
alflennik <alflennik@users.noreply.github.com>
2023-06-02 19:43:16 -0400

    Update songs

---
+++ playlists/2023-01-01-love-letter/03-the-blaze-dreamer.js
@@ -0,0 +1,79 @@
+export default {
+  descriptions: [
+    {
+      time: 2.26,
+      text: "Six black men are perched on a tower, reminiscent in shape to the Eiffel Tower, albeit a lot smaller, only like thirty feet tall."
+    },
+    {
+      time: 11.056,
+      text: "Wind whips their clothes. Seabirds fly above. "
+    },
+    {
+      time: 19.095,
+      text: "The men hold on to the tower with one hand, hold the other into the air like they could almost fly."
+    },
+    {
+      time: 28.261,
+      text: "Their world is not so different from the more frequently portrayed worlds to the North and West. There is a market, a port, development all across the coastline."
+    },
+    {
+      time: 54.665,
+      text: "One of the men pumps his fist into the air like a challenge directed at the universe. There is something about his expression, he has an intensity. This man is awake."
+    },
+    {
+      time: 75.061,
+      text: "He walks with his brothers along the coastline."
+    },
+    {
+      time: 90.664,
+      text: "Dozens of men and boys are arranged on the beach, hopping up and down, left and right, in vigorous exercise. The dreamer is there, pushing himself to his limit."
+    },
+    {
+      time: 118.667,
+      text: "Chewing on a bit of sugarcane, shirt open to cope with the heat, he skateboards with his bros to a gathering. With ladies laughing along nearby, he balances his kid on his shoulders."
+    },
+    {
+      time: 139.037,
+      text: "In a bedroom, he's in bed with his girl, leaning in for an intense kiss."
+    },
+    {
+      time: 154.375,
+      text: "Out at the beach, he watches the waves crash with his boy."
+    },
+    {
+      time: 172.39,
+      text: "And he makes it to a wired up room recognizable as a recording studio. And he sings a song."
+    },
+    {
+      time: 208.34,
+      text: "His crew of six guys are squeezed into a booth meant for one, singing backup vocals."
+    },
+    {
+      time: 229.091,
+      text: "The dreamer bobs his head strenuously, hyping everyone up."
+    },
+    {
+      time: 246.388,
+      text: "Still on the rush, he blows hookah smoke and parties with heaps of friends at a pool."
+    },
+    {
+      time: 264.021,
+      text: "They splash water, leap in, frolic around joyously."
+    },
+    {
+      time: 279.283,
+      text: "The dreamer stands above everyone else, hoised up by his buddies."
+    },
+    {
+      time: 290.366,
+      text: "They cheer, they rock out till sunrise."
+    },
+    {
+      time: 325.942,
+      text: 'And his kid still watches the waves. Title reads, "Dreamer."'
+    }
+  ],
+  analysis: {
+    text: "Analysis: The Blaze is a French duo, two white guys, and their aim is to show how their brothers down in Africa are not so different. This might be considered condescending in the US, but I don't know, I think it kind of lands. What do you think?"
+  }
+}
--- playlists/2023-01-01-love-letter/contents.js
+++ playlists/2023-01-01-love-letter/contents.js
@@ -3,17 +3,6 @@ export default {
   title: "Love Letter",
   videos: [
     {
-      id: "the-blaze-dreamer",
-      artist: "The Blaze",
-      title: "DREAMER",
-      videoType: "musicVideo",
-      thumbnailSrc:
-        "https://i.ytimg.com/vi/G6K_hkVdQgw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCiwnbqQ7CZO9tBBGZNQHrK9Fizxg",
-      thumbnailAlt: "TODO",
-      youTubeId: "G6K_hkVdQgw",
-      descriptionPath: "/playlists/2022-10-31-love-letter/03-the-blaze-dreamer.js",
-    },
-    {
       id: "lute-finding-self",
       artist: "Lute",
       title: "Finding Self",
@@ -23,20 +12,30 @@ export default {
         "https://i.ytimg.com/vi/FRGJnz6w4-A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDfl73HlpJXjaAtk-xFp62ZeWC_4w",
       thumbnailAlt: "TODO",
       youTubeId: "FRGJnz6w4-A",
-      descriptionPath: "/playlists/2022-10-31-love-letter/01-lute-finding-self.js",
+      descriptionPath: "/playlists/2022-10-31-love-letter/01-lute-finding-self.js"
     },
     {
       id: "salvatore-ganacci-take-me-to-america",
       artist: "Salvatore Ganacci",
       title: "Take Me To America",
       videoType: "musicVideo",
-      videoTypeCustomLabel: "Short Film",
       thumbnailSrc:
         "https://i.ytimg.com/vi/PcR8I-7oCnU/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIF0oTzAP&rs=AOn4CLDXPEoxfcKsC9DcokOOUAT7gHXR6w",
       thumbnailAlt: "TODO",
       youTubeId: "PcR8I-7oCnU",
       descriptionPath:
-        "/playlists/2022-10-31-love-letter/02-salvatore-ganacci-take-me-to-america.js",
+        "/playlists/2022-10-31-love-letter/02-salvatore-ganacci-take-me-to-america.js"
     },
-  ],
+    {
+      id: "the-blaze-dreamer",
+      artist: "The Blaze",
+      title: "DREAMER",
+      videoType: "musicVideo",
+      thumbnailSrc:
+        "https://i.ytimg.com/vi/G6K_hkVdQgw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCiwnbqQ7CZO9tBBGZNQHrK9Fizxg",
+      thumbnailAlt: "TODO",
+      youTubeId: "G6K_hkVdQgw",
+      descriptionPath: "/playlists/2022-10-31-love-letter/03-the-blaze-dreamer.js"
+    }
+  ]
 }
