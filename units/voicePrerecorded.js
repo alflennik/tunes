adaea0fe323a452a2ab7068678f025d1ae7cab71
alflennik <alflennik@users.noreply.github.com>
2023-10-06 18:49:53 -0400

    Fix permissions issue for prerecorded voice

--- units/voicePrerecorded.js
+++ units/voicePrerecorded.js
@@ -7,42 +7,44 @@ define("voicePrerecorded", {
 
   update: function ({ change }) {
     getPermissions = once($getPermissions, () => {
-      const audioFilePaths = this.descriptions.map(description => description.filePath)
-      audioFilePaths.push(this.analysis.filePath)
-      const newAudioElements = []
-      audioFilePaths.map(audioFilePath => {
-        const audioElement = new Audio(audioFilePath)
-        newAudioElements.push(audioElement)
-      })
-
-      change(() => {
-        this.audioElements = newAudioElements
-        this.playMode = "unstarted"
+      return new Promise(resolve => {
+        const audioElement = new Audio("../../audio/none.mp3")
+        audioElement.addEventListener("ended", () => {
+          resolve()
+        })
+        audioElement.play()
+        change(() => {
+          this.playMode = "unstarted"
+        })
       })
     })
 
     say = once($say, async spokenItem => {
-      const audioElement = this.audioElements.find(
-        audioElement => audioElement.getAttribute("src") === spokenItem.filePath
-      )
+      const audioElement = new Audio(spokenItem.filePath)
+
       if (this.activePromise && !this.activeElement.paused) {
         await this.activePromise
       }
-      this.activeElement = audioElement
-      this.activePromise = new Promise(resolve => {
-        this.activeElement.addEventListener("ended", async () => {
-          await change(() => {
-            this.activeElement = null
-            this.activePromise = null
-            this.playMode = "ended"
-          })
-          resolve()
-        })
-      })
+
       await change(() => {
-        this.activeElement.play()
+        this.activeElement = audioElement
+        this.activePromise = new Promise(resolve => {
+          const endListener = async () => {
+            await change(() => {
+              this.activeElement = null
+              this.activePromise = null
+              this.playMode = "ended"
+            })
+            this.activeElement.removeEventListener("ended", endListener)
+            resolve()
+          }
+          this.activeElement.addEventListener("ended", endListener)
+        })
         this.playMode = "playing"
       })
+
+      this.activeElement.play()
+
       return this.activePromise
     })
 
