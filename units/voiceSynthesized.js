0b9323a2aa4182087c61d4602fb7bc19b896afd2
alflennik <alflennik@users.noreply.github.com>
2024-06-08 17:35:50 -0400

    Update voice rates

--- units/voiceSynthesized.js
+++ units/voiceSynthesized.js
@@ -30,7 +30,7 @@ define("voiceSynthesized", {
             // ["Eddy (English (US))", 1.3],
             // ["Evan (Enhanced)", 1], // Must be explicitly downloaded
             // ["Alex", 1], // Must be explicitly downloaded
-            ["Samantha", isSafari ? 1.1 : 1],
+            ["Samantha", isChrome ? 1.5 : isSafari ? 1.15 : 1.1],
 
             // Windows
             ["Microsoft Steffan Online (Natural) - English (United States)", 1.7], // Edge only
