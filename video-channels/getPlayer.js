35adbc67f079c4572a925a9944d35030eb517f55
alflennik <alflennik@users.noreply.github.com>
2024-06-19 15:47:14 -0400

    Add description button

--- video-channels/getPlayer.js
+++ video-channels/getPlayer.js
@@ -63,6 +63,53 @@ const getPlayer = async ({ videos, channels, startsMuted }) => {
         font-weight: bold;
         border-radius: 4px;
       }
+      .descriptions-button-container {
+        float: right;
+        display: flex;
+      }
+      .descriptions-button {
+        display: flex;
+        align-items: center;
+        gap: 0;
+        background: #242424;
+        border-radius: 4px;
+        padding: 3px 18px 7px 21px;
+        border-radius: 4px 0 0 4px;
+        transition: background 200ms;
+      }
+      .descriptions-button:hover {
+        background: #333;
+      }
+      .descriptions-button svg {
+        width: 18px;
+        height: 18px;
+        fill: white;
+        transition: fill 200ms;
+      }
+      .descriptions-button.on {
+        background: white;
+      }
+      .descriptions-button.on svg {
+        fill: black;
+      }
+      .descriptions-status {
+        background: #4c4c4c;
+        padding: 4px 8px;
+        display: flex;
+        align-items: center;
+        border-radius: 0 4px 4px 0;
+      }
+      .descriptions-status svg {
+        height: 14px;
+        width: 14px;
+        fill: #ffffff;
+      }
+      .descriptions-status.available {
+        background: #55f243;
+      }
+      .descriptions-status.available svg {
+        fill: #000;
+      }
     </style>
 
     <div class="player-container">
@@ -116,18 +163,73 @@ const getPlayer = async ({ videos, channels, startsMuted }) => {
           </svg>
           <span class="sr-only">Next</span>
         </button>
-        <a class="button-link" href="../" title="Audio Descriptions" style="float: right;">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-            <path
-              d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h56 64 16c17.7 0 32-14.3 32-32s-14.3-32-32-32H384V64H576V256H384V224H320v48c0 26.5 21.5 48 48 48H592c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H368c-26.5 0-48 21.5-48 48v80H243.1 177.1c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9V480c0 17.7 14.3 32 32 32s32-14.3 32-32z"
-            />
-          </svg>
-          <span class="sr-only">Audio Descriptions</span>
-        </a>
+
+        <div style="display: none;" class="descriptions-button-container">
+          <button class="descriptions-button" aria-described-by="descriptions-status">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+              <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+              <path
+                d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h56 64 16c17.7 0 32-14.3 32-32s-14.3-32-32-32H384V64H576V256H384V224H320v48c0 26.5 21.5 48 48 48H592c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H368c-26.5 0-48 21.5-48 48v80H243.1 177.1c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9V480c0 17.7 14.3 32 32 32s32-14.3 32-32z"
+              ></path>
+            </svg>
+            <span class="sr-only"></span>
+          </button>
+          <div id="descriptions-status" class="descriptions-status"></div>
+        </div>
       </div>
     </div>
   `
+
+  const descriptionsButton = document.querySelector(".descriptions-button")
+  const toggleDescriptionsButton = () => {
+    if (descriptionsButton.classList.contains("off")) {
+      descriptionsButton.classList.add("on")
+      descriptionsButton.classList.remove("off")
+      descriptionsButton.setAttribute("title", "Descriptions On")
+      descriptionsButton.querySelector(".sr-only").innerHTML = "Descriptions On"
+    } else {
+      descriptionsButton.classList.remove("on")
+      descriptionsButton.classList.add("off")
+      descriptionsButton.setAttribute("title", "Descriptions Off")
+      descriptionsButton.querySelector(".sr-only").innerHTML = "Descriptions Off"
+    }
+  }
+  toggleDescriptionsButton()
+  descriptionsButton.addEventListener("click", () => {
+    toggleDescriptionsButton()
+  })
+
+  const descriptionsStatus = document.querySelector(".descriptions-status")
+  const toggleDescriptionsStatus = () => {
+    if (descriptionsStatus.classList.contains("unavailable")) {
+      descriptionsStatus.classList.add("available")
+      descriptionsStatus.classList.remove("unavailable")
+      descriptionsStatus.setAttribute("title", "Available")
+      descriptionsStatus.innerHTML = /* HTML */ `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+          <path
+            d="M342.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 178.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l80 80c12.5 12.5 32.8 12.5 45.3 0l160-160zm96 128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 402.7 54.6 297.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l256-256z"
+          />
+        </svg>
+        <span class="sr-only">Available</span>
+      `
+    } else {
+      descriptionsStatus.classList.remove("available")
+      descriptionsStatus.classList.add("unavailable")
+      descriptionsStatus.setAttribute("title", "Unavailable for this video")
+      descriptionsStatus.innerHTML = /* HTML */ `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+          <path
+            d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
+          ></path>
+        </svg>
+        <span class="sr-only">Unavailable for this video</span>
+      `
+    }
+  }
+  toggleDescriptionsStatus()
 
   const formatChannel = () => {
     if (channels.getCurrentChannel() < 9) return `0${channels.getCurrentChannel() + 1}`
