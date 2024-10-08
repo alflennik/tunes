7f599016758ee62f2596c518d30543acf19e3338
alflennik <alflennik@users.noreply.github.com>
2024-10-08 11:53:20 -0400

    Kirinji is it v3 or v4 now

--- videos/kirinji-twilight.js
+++ videos/kirinji-twilight.js
@@ -2,35 +2,35 @@ export default {
   descriptions: [
     {
       time: 0.242,
-      text: 'Titles give the artist "Kirinji" and the song title, "Twilight", in Japanese.',
+      text: 'Titles read "Twilight," by "Kirinji," featuring Maika Loubté.',
     },
     {
-      time: 9.086,
-      text: "The endless city is full of bewildering lights. Signs point left, right and center. A car accelerates. It's late enough for a huge ferris wheel to have stopped for the night, but not late enough to cease its dazzling pattern of lights.",
+      time: 12.412,
+      text: "Tokyo at night, an endless labyrinth. Barriers, lights, indicators, warnings and signs all point in different directions. The path forward is far from clear.",
     },
     {
       time: 29.457,
-      text: "A young man drives his car through the dark city, and a young woman walks despondently along the waterside. Her face is puffy as if she's been crying.",
+      text: "A young man in his car searches for something, or someone.",
     },
     {
-      time: 47.868,
-      text: "The man driving scans the side of the road. Elsewhere the woman looks over her shoulder, glances at her phone.",
+      time: 45.259,
+      text: "A young woman walks despondent along the waterside. Her face is puffy as if she's been crying.",
     },
     {
-      time: 96.661,
-      text: "The artists are very much embedded in the urban isolation - Maika Loubté is in a hoodie and singing from her seat in a city bus, and Kirinji sings from a park bench, ferris wheel at his back, foot tapping absentmindedly along with the beat.",
+      time: 60.416,
+      text: "The young man continues searching, unable to find the young woman who leaves her messages unanswered and treks deeper and deeper into the city.",
     },
     {
-      time: 109.93,
-      text: "The woman gazes with longing at a toy carousel, which has no right to be there and casts her face in a sickly light.",
+      time: 97.723,
+      text: "The artists are embedded in this urban isolation. Kirinji sits on a park bench, the massive Yokohama ferris wheel at his back. Maika Loubté, in a hoodie, rides a city bus through the Tokyo labyrinth.",
     },
     {
       time: 125.003,
-      text: "Caption reads: 'We do this so that the carousel never stops rotating.'",
+      text: 'Caption reads, "We do this so that the carousel never stops rotating." And the woman is fixated on a toy carousel, a toy that somehow does not seem out of place inside such vertigo and spinning chaos of this endless metropolis.',
     },
     {
-      time: 141.864,
-      text: "The man continues his search. The woman walks up a pedestrian overpass, down an escalator. Maika anxiously clutches her neck with both arms.",
+      time: 156.774,
+      text: "Trapped in an unceasing, unthinking cycle, the man searches, the woman treads away.",
     },
     {
       time: 175.952,
@@ -38,31 +38,30 @@ export default {
     },
     {
       time: 179.349,
-      text: "Maika sings with determination, her head nodding, her eyes distant, as if remembering something important.",
+      text: "Maika sings with determination, her head nodding, as if remembering something important.",
     },
     {
       time: 193.897,
-      text: "Caption reads: 'The stars you gaze upon. Tell us so we can sing.'",
+      text: 'Caption reads: "The stars you gaze upon. Tell us so we can sing."',
     },
     {
-      time: 198.123,
-      text: "The woman stops and starts to sway. Maika drops the hood of her hoodie and smiles, as if a burden has been relieved.",
+      time: 201.563,
+      text: "Maika drops the hood of her hoodie and smiles, as if a burden has been relieved.",
     },
     {
-      time: 210.433,
-      text: "The woman looks into the starless sky and a smile appears.",
-    },
-    {
-      time: 216.638,
-      text: "She spins in a reckless dance, swinging her purse. For a moment she is still. And then she is turning. She holds her arms out. Her mouth is open in a laughing smile.",
+      time: 210.895,
+      text: "The young woman, somewhere on the seaside promenade, suddenly stops. And walks forward. And stops again. A miraculous new ability. She spins in a reckless dance, swinging her purse. She holds her arms out. Her mouth is open in a laughing smile.",
     },
     {
       time: 231.979,
-      text: "It's a languorous dance of someone who nearly forgot but now remembers. No fate.",
+      text: "It's a languorous dance of someone set free.",
     },
     {
       time: 260.694,
-      text: "Darkness having lifted with the arrival of dawn, she crosses a bridge, ferris wheel in the background. A passing car's headlights shine, the car driven by the man. The woman notices him. She smiles a secret smile and walks in the opposite direction.",
+      text: "Dawn has broken. The woman crosses in front of the massive ferris wheel, and she notices the car driven by the man. She considers whether to yell for his attention. But she turns. She goes her own way.",
     },
   ],
+  analysis: {
+    text: 'Analysis: Again and again Kirinji sings, "no fate." When you are trapped in a spinning chaos of thoughts, it doesn\'t matter what you choose. But you must make your choice.',
+  },
 }
